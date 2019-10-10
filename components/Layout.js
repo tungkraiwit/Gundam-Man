8edc7d5a4ee4166cdfa0df52118be37253551981
sasirat maneejian <sasirat019@gmail.com>
2019-10-10 20:37:07 +0700

    Add new component content

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,14 @@ import MainMenu from '../components/MainMenu'
 class Layout extends React.Component {
   render() {
     return (
-      <div style={{width: '300px', position: 'fixed', height: '-webkit-fill-available',background:'#204F5F'}}>
-        <MainMenu />
+      <div className="columns">
+        <div className="column" style={{width: '300px', position: 'fixed', height: '-webkit-fill-available',background:'#204F5F'}}>
+          <MainMenu />
+        </div>
+        <div className="column" style={{ marginLeft: '300px' }}>
+          {this.props.children}
+        </div>
+
       </div>
     )
   }
