e014da42e4c09e2caa2d75b605725659c2332184
sasirat maneejian <sasirat019@gmail.com>
2019-10-15 19:57:54 +0700

    Update catalog page

--- pages/catalogs.js
+++ pages/catalogs.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import MainMenu from '../components/MainMenu'
+// import MainMenu from '../components/MainMenu'
 import Layout from '../components/Layout'
 import Content from '../components/content'
 
+
 class Catalogs extends React.Component {
   render() {
+    // const users = require('@sasirat/project-database').getCustomerNumber()
+    // const users = db.getCustomerNumber()
     return (
       <div>
         <Layout>
         <Content />
+        {/* {users} */}
         </Layout>
       </div>
     )
