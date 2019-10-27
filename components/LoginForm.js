f59fe549e680190471f8e961ff2eec8124442ad0
sasirat maneejian <sasirat019@gmail.com>
2019-10-27 17:46:24 +0700

    Revert "Update LoginForm.js"
    This reverts commit 32ae646a073373ed4d682ef3da0a7e32a986c572.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,68 +1,29 @@
 import React from 'react'
 
 class LoginForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      email: '',
-      password: ''
-      // currentUser: null,
-      // message: ''
-    }
-  }
-
-  onChange = e => {
-    const { name, value } = e.target
-
-    this.setState({
-      [name]: value
-    })
-  }
-
-  onSubmit = e => {
-    e.preventDefault()
-
-    const { email, password } = this.state
-    // TODO: implement signInWithEmailAndPassword()
-  }
-
   render() {
     return (
       <section className="section container">
         <div className="columns is-centered">
           <div className="column is-half">
-            <form onSubmit={this.onSubmit}>
+            <form>
               <div className="field">
-                <label className="label">Email</label>
+                <label className="label" >User Name</label>
                 <div className="control">
-                  <input
-                    className="input"
-                    type="email"
-                    name="email"
-                    onChange={this.onChange}
-                  />
+                  <input className="input" type="name" name="name" />
                 </div>
               </div>
 
               <div className="field">
-                <label className="label">Password</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="password"
-                    name="password"
-                    onChange={this.onChange}
-                  />
+                <label className="label">Password</label>                
+                <div className="control">                  
+                  <input className="input" type="password" name="password" />
                 </div>
               </div>
 
               <div className="field is-grouped">
-                <div className="control">
-                  <button className="button is-link">Submit</button>
-                </div>
-                <div className="control">
-                  <button className="button is-text">Cancel</button>
+                <div className="control" >
+                  <button className="button is-link">Sign In</button>
                 </div>
               </div>
             </form>
@@ -72,4 +33,5 @@ class LoginForm extends React.Component {
     )
   }
 }
+
 export default LoginForm
