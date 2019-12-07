39161ddf76e68f78230714b548de80f1a5cf451c
sasirat maneejian <sasirat019@gmail.com>
2019-12-07 07:24:12 +0700

    Initialize add member

---
+++ pages/member.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Content from '../components/content'
+import Fetch from '../components/fetch'
+
+class Member extends React.Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Content title="Add member" />
+          
+          
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default Member
