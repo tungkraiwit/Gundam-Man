78f366a32fb0fc8f38c4ff8d71530d97f2ad3b14
sasirat maneejian <sasirat019@gmail.com>
2019-12-08 13:44:29 +0700

    Add form in add member

--- pages/member.js
+++ pages/member.js
@@ -9,8 +9,80 @@ class Member extends React.Component {
       <div>
         <Layout>
           <Content title="Add member" />
-          
-          
+          <div style={{ margin: "300px", marginTop: "100px", marginLeft: "100px" }}>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">Name</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="text" placeholder="Name" style={{ padding: "0.5rem" }} />
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">Contact name</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="text" placeholder="Firstname" style={{ padding: "0.5rem" }} />
+                  </p>
+                </div>
+                <div class="field">
+                  <p class="control is-expanded">
+                    <input class="input" type="text" placeholder="Lastname" />
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">Phone</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="text" placeholder="Phone number" style={{ padding: "0.5rem" }} />
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">Address1</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <textarea class="textarea" placeholder="Address1"></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label">Address2</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <textarea class="textarea" placeholder="Address2"></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
         </Layout>
       </div>
     )
