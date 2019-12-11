771f51c698542b63b76936c0d84561e45cfdd4cc
sasirat maneejian <sasirat019@gmail.com>
2019-12-11 09:38:12 +0700

    Add filter in product

---
+++ components/scale-bar.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import { Dropdown, DropdonwMenu } from '../components/dropdown'
+import Api from '../api'
+import Fetch from './fetch'
+import { async } from 'q'
+
+const ScaleBar = ({ onChange }) => {
+  // const option = ["Min Lin Diecast","Classic Metal Creations","Highway 66 Mini Classics","Red Start Diecast","Motor City Art Classics","Second Gear Diecast","Autoart Studio Design","Welly Diecast Productions","Unimax Art Galleries","Studio M Art Models","Exoto Designs","Gearbox Collectibles","Carousel DieCast Legends"]
+  const [vendors, setVendors] = useState([])
+  const [scales, setScales] = useState([])
+  const [vendor, setVendor] = useState(undefined)
+  const [scale, setScale] = useState(undefined)
+
+  useEffect(async () => {
+    const { data } = await Api.get('/products/vendor')
+    setVendors(data)
+  }, []);
+  useEffect(async () => {
+    const { data } = await Api.get('/products/scale')
+    setScales(data)
+  }, []);
+  // send value out
+  useEffect(() => {
+    onChange(vendor, scale)
+  })
+
+  return (
+    <div className="columns" style={{ margin: "50px", marginTop: "0px", marginBottom: "20px" }}>
+      <div className="column is-4" style={{ display: 'flex' }}>
+        <div style={{ margin: 'auto 0.5rem' }}>
+          <h1>Vendor : </h1>
+        </div>
+        <Dropdown title="Select vendor" placeholder={vendor}>
+          <DropdonwMenu
+            list={vendors}
+            onClick={value => {
+              setVendor(value)
+            }}
+          />
+        </Dropdown>
+      </div>
+      <div className="column" style={{ display: 'flex' }}>
+        <div style={{ margin: 'auto 0.5rem' }}>
+          <h1>Scale : </h1>
+        </div>
+        <Dropdown title="Select Scale" placeholder={scale}>
+          <DropdonwMenu
+            list={scales}
+            onClick={(value) => {
+              setScale(value)
+            }}
+          />
+        </Dropdown>
+      </div>
+    </div>
+  )
+}
+
+export default ScaleBar
--- pages/product.js
+++ pages/product.js
@@ -1,99 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/Layout'
 import Content from '../components/content'
-import Fetch from '../components/fetch'
-import Popup from 'reactjs-popup'
+import ScaleBar from '../components/scale-bar'
+import Api from '../api'
 
-class Catalogs extends React.Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Content title="Product" />
-          <table className="table" style={{margin:"70px",marginTop:"0px"}}>
-            <thead>
+const Catalogs = () => {
+  const [vendor, setVendor] = useState(undefined)
+  const [products, setProducts] = useState([])
+  const [scale, setScale] = useState(undefined)
+  // fetch products
+  useEffect(async () => {
+    const { data } = await Api.get('/products')
+    setProducts(data)
+  }, [])
+
+  return (
+    <div>
+      <Layout>
+        <Content title="Product" />
+        <ScaleBar
+          onChange={(vendor, scale) => {
+            setVendor(vendor)
+            setScale(scale)
+          }}
+        />
+        <table className="table" style={{ margin: "70px", marginTop: "0px" }}>
+                <thead>
+                  <tr>
+                    <th align="center">Product code</th>
+                    <th align="center">Product Name</th>
+                    <th align="center">Product Line</th>
+                    <th align="center">Product Scale</th>
+                    <th align="center">Product Vendor</th>
+                    <th align="center">Product Description</th>
+                    <th align="center">quantity In Stock</th>
+                    <th align="center">buyPrice</th>
+                    <th align="center">MSRP</th>
+                  </tr>
+                </thead>
+          
+        {
+          // filter
+          products.filter(product => {
+            if (!vendor) return true
+            return product.product_vendor === vendor
+          }).filter((product) => {
+            if (!scale) return true
+            return product.product_scale === scale
+          }).map(product => {
+          return (
+            <tbody>
               <tr>
-                <th align="center">Product Code</th>
-                <th align="center">Product Name</th>
-                <th align="center">Product Line</th>
-                <th align="center">Product Scale</th>
-                <th align="center">Product Vendor</th>
-                <th align="center">Product Description</th>
-                <th align="center">quantity In Stock</th>
-                <th align="center">buyPrice</th>
-                <th align="center">MSRP</th>
-               </tr>
-              </thead>
-              <Fetch url="/products">
-                {data => {
-                  return data.map(product => (
-                    <tbody>
-                    <tr>
-                      <td>
-                        {product.product_code}
-                      </td>
-                      <td>
-                        {product.product_name}
-                      </td>
-                      <td>
-                        {product.product_line}
-                      </td>
-                      <td>
-                        {product.product_scale}
-                      </td>
-                      <td>
-                        {product.product_vendor}
-                      </td>
-                      <td>
-                        {product.product_description}
-                      </td>
-                      <td>
-                        {product.quantity_instock}
-                      </td>
-                      <td>
-                        {product.buy_price}
-                      </td>
-                      <td>
-                        {product.msrp}
-                      </td>
-                      <Popup
-                          trigger={<button> more </button>}
-                          modal
-                        >
-                          {close => (
-                            <span>
-                              {/* <button
-                                  className="button is-pulled-right"
-                                  onClick={() => {close()}}
-                                >
-                                  x
-                                </button> */}
-                              <div style={{margin:"50px"}}>
-                                <p>productCode        : {product.product_code}</p>
-                                <p>productName        : {product.product_name}</p>
-                                <p>productLine        : {product.product_line}</p>
-                                <p>productScale       : {product.product_scale}</p>
-                                <p>productVendor      : {product.product_vendor}</p>
-                                <p>productDescription : {product.product_description}</p>
-                                <p>quantityInStock    : {product.quantity_instock}</p>
-                                <p>buyPrice           : {product.buy_price}</p>
-                                <p>MSRP               : {product.msrp}</p>
-                                <p>textDescription    : {product.text_description}</p>
-                              </div>
-                                <button className="button is-pulled-right is-rounded" style={{ margin: "50px", marginTop: "0px",marginLeft:"0px" ,background: "#F19671", color: "#FFFFFF" }} onClick={() => {close()}}>back</button>
-                            </span>
-                          )}
-                        </Popup>
-                    </tr>
-                    </tbody>
-                  ))
-                }}
-              </Fetch>
-          </table>
-        </Layout>
-      </div>
-    )
-  }
+                <td>{product.product_code}</td>
+                <td>{product.product_name}</td>
+                <td>{product.product_line}</td>
+                <td>{product.product_scale}</td>
+                <td>{product.product_vendor}</td>
+                <td>{product.product_description}</td>
+                <td>{product.quantity_instock}</td>
+                <td>{product.buy_price}</td>
+                <td>{product.msrp}</td>
+              </tr>
+
+            </tbody>
+          )
+          })
+        }
+        </table>
+      </Layout>
+    </div>
+  )
 }
 
 export default Catalogs
