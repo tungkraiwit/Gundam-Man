cd29e23a3fe8a4cfa04e957508f45ea350aafcaa
sasirat maneejian <sasirat019@gmail.com>
2019-12-11 16:55:47 +0700

    Add edit modal

---
+++ components/form-employee.js
@@ -0,0 +1,114 @@
+import React from 'react'
+
+class FormEmployee extends React.Component {
+  render (){
+    return (
+      <div className="content">
+        <div style={{ margin: "50px" }}>
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">employee numbner</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="Product Code" class="input" type="text" placeholder="employee number" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">First name</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="Product Name" class="input" type="text" placeholder="first Name" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">last name</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="Product Line" class="input" type="text" placeholder="last name" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">extention</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="Product Scale" class="input" type="text" placeholder="extention" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">email</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="Product Vendor" class="input" type="email" placeholder="email" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">office code</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="Product Vendor" class="input" type="text" placeholder="office code" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">report to</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="quantity In Stock" class="input" type="text" placeholder="report to" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">job title</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input onChange={this.onChange} name="buyPrice" class="input" type="text" placeholder="job title" style={{ padding: "0.5rem" }} />
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <button onClick={this.onSubmit} className="button is-pulled-right is-rounded" style={{ marginRight: "50px", marginTop: "0px", background: "#F19671", color: "#FFFFFF" }} >Save</button>
+      </div>
+    )
+  }
+}
+
+export default FormEmployee
