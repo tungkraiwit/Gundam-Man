497d9a191c1d8e761e8542f401c48071feebdd4c
tungkraiwit <kraiwitkeyen1998@gmail.com>
2019-12-11 21:34:05 +0700

    add in herforder

--- pages/testpage.js
+++ pages/testpage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Page from '../components/form-product'
+import Api from '../api'
 
 class testOne extends React.Component {
+  async onClick() {
+    const CustomerName ='Australian Collectors, Co.'
+    const dadadada = await Api.post('/orders', { data:  CustomerName})
+    console.log(dadadada.data)
+  }
   render() {
     return (
       <div>
-        <Page />        
+        <button onClick={this.onClick}>clickkkkkkk</button>
       </div>
     )
 
